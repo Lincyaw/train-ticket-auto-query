3e1027cca05b647cb8a12109e02b8be904c7d845
Yimou Wu <yimouwu@link.cuhk.edu.cn>
2024-05-05 22:38:23 +0800

    change the base address

--- query_admin_basic_price.py
+++ query_admin_basic_price.py
@@ -11,7 +11,7 @@ date = time.strftime("%Y-%m-%d", time.localtime())
 # base_address = "http://139.196.152.44:31000"
 # base_address = "http://10.107.91.15:30729"
 # base_address = "http://10.10.10.201:32075"
-base_address = "http://10.106.225.127:32075"
+base_address = "http://10.106.225.127:32059"
 
 
 def query_admin_basic_price(headers):
