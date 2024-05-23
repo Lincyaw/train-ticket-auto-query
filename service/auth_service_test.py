de32d3cd0a474a36569fafba281137f8db72a30b
Yimou Wu <yimouwu@link.cuhk.edu.cn>
2024-05-23 07:29:51 +0000

    admin_basic_info_service

--- service/auth_service_test.py
+++ service/auth_service_test.py
@@ -1,16 +1,5 @@
 from service.auth_service import *
-
-BASE_URL = "http://10.10.10.220:30193"
-headers = {
-    'Proxy-Connection': 'keep-alive',
-    'Accept': 'application/json',
-    'X-Requested-With': 'XMLHttpRequest',
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36',
-    'Content-Type': 'application/json',
-    'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
-    'Connection': 'keep-alive',
-}
-
+from service.test_utils import *
 
 def test_get_auth_hello():
     client = requests.Session()
